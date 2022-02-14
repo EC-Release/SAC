a5d43caf49c50f8d78304661364f0cbfa706179b
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-14 08:35:30 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@
 use std::env;
 
 //#![feature(proc_macro_hygiene, decl_macro)]
-//#[macro_use] extern crate rocket;
-extern crate rocket;
+#[macro_use] extern crate rocket;
 
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
