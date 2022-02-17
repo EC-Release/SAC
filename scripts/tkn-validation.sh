bbb4681165a521f886513600112b8fc04e749406
Puja129 <pujashar9@gmail.com>
2022-02-17 14:05:48 -0500

    Created token validation script skeleton[ES1XM-2]
    Created skeleton for token validation script [ES1XM-2]

--- scripts/tkn-validation.sh
+++ scripts/tkn-validation.sh
@@ -8,3 +8,21 @@
 #  with the terms and conditions stipulated in the agreement/contract
 #  under which the software has been supplied.
 #
+# verify the Cognito token and validate service-id from the map
+# $1: <EXEC_DAT>
+# $2: <EXEC_MAP>
+function int_a () {
+
+  if [[ -z $EXEC_DAT ]]; then
+    printf "empty request."
+    return
+  else
+    STATUS=$(curl -s -o /dev/null -w "%{http_code}" https://cognito-idp.$region.amazonaws.com/$userpool/.well-known/jwks.json)
+    if [ $STATUS -eq 200 ]; then
+      echo "Got 200! proceeding to next step..."
+    else
+      echo "Got $STATUS : invalid token"
+      return      
+  fi
+  
+}
