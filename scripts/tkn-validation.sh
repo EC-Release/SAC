6e431c79999e2fd891cf31669bd8944f30343486
Puja129 <pujashar9@gmail.com>
2022-02-28 14:37:46 -0500

    Update tkn-validation.sh

--- scripts/tkn-validation.sh
+++ scripts/tkn-validation.sh
@@ -30,14 +30,14 @@ function int_a () {
     #printf "%s" "{\"message\":\"service-id exists in the map, proceeding to next step\"}"
     jwtdec=$ jq -R 'split(".") | .[0] | @base64d | fromjson' <<< "$token"
     #jwtdec2=$(echo "$token" | awk -F'.' '{print $3}')
-    jwtdec2=$ jq -R '.kid' <<< "$jwtdec"
+    #jwtdec2=$ jq -R '.kid' <<< "$jwtdec"
     #printf "%s" "$jwtdec"
     #kid=$(echo "$jwtdec" | jq -r '.kid')
     #kid=$(grep $kid $jwtdec | cut -d ":" -f2-)
     #kid=$(grep -oP '"kid":*'  <<< "$jwtdec")
     kid=$(echo "$jwtdec2" | cut -d':' -f2-)
     #printf "%s" "$kid"
-    printf "{\"userpool\":%s,\"jwtdec\":%s,\"kid\":%s}" "$userpool" "$jwtdec2" "$kid"
+    printf "{\"userpool\":%s,\"jwtdec\":%s,\"kid\":%s}" "$userpool" "$jwtdec" "$kid"
     #if [[ ! -z $jwtdec ]]; then
     #  kid=$(echo $jwtdec | jq -r '.kid')
     #  printf "%s" "$kid"
