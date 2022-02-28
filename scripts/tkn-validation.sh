9670ee432f6b125b07f92347c723b17950bf83fd
Puja129 <pujashar9@gmail.com>
2022-02-28 12:27:08 -0500

    Update tkn-validation.sh

--- scripts/tkn-validation.sh
+++ scripts/tkn-validation.sh
@@ -30,9 +30,9 @@ function int_a () {
     #printf "%s" "{\"message\":\"service-id exists in the map, proceeding to next step\"}"
     jwtdec=$ jq -R 'split(".") | .[0] | @base64d | fromjson' <<< "$token"
     #printf "%s" "$jwtdec"
-    ki=$(echo "$jwtdec" | jq -r '.kid')
+    kid=$(echo "$jwtdec" | jq -r '.kid')
     #ki=$(grep $kid $jwtdec | cut -d ":" -f2-)
-    printf "%s" "$ki"
+    printf "%s" "$kid"
     #printf "{\"userpool\":%s,\"ki\":%s}" "$userpool" "$ki"
     #if [[ ! -z $jwtdec ]]; then
     #  kid=$(echo $jwtdec | jq -r '.kid')
