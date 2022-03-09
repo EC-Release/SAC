74344a6ed5d138afb27eb5607e97e321cd0f6ed9
Puja129 <pujashar9@gmail.com>
2022-03-09 09:14:39 -0500

    Update tkn-validation.sh

--- scripts/tkn-validation.sh
+++ scripts/tkn-validation.sh
@@ -18,10 +18,11 @@ function int_a () {
   if [[ -z $1 ]]; then
     printf "%s" "{\"error\":\"empty request\",\"decision\":\"DENY\"}"
   fi
-  region=$(echo $1 | jq -r '.region')
+  #region=$(echo $1 | jq -r '.region')
   svcId=$(echo $1 | jq -r '.svcId')
   token=$(echo $1 | jq -r '.token')
-  if [[ -z $region || -z $svcId || -z $token ]]; then
+  #if [[ -z $region || -z $svcId || -z $token ]]; then
+  if [[ -z $svcId || -z $token ]]; then
     printf "%s" "{\"error\":\"required parameters missing in the request\",\"decision\":\"DENY\"}"
   fi  
   cog_url=$(echo $2 | jq -r '.COGNITO_URL')
@@ -30,7 +31,9 @@ function int_a () {
   if [[ $res != "0" ]]; then
     printf "%s" "{\"error\":\"service-id does not exist in the map.\",\"decision\":\"DENY\"}"
   else
-    userpool=$(echo "$map" | cut -d':' -f1 | cut -d'"' -f2 | cut -d'"' -f1)    
+    userpool=$(echo "$map" | cut -d':' -f1 | cut -d'"' -f2 | cut -d'"' -f1) 
+    region=$(echo "$userpool" | cut -d'_' -f1) 
+    printf "{\"region\":\"%s\"}" "$region"
     jwtdec=$(echo "$token" | jq -R 'split(".") | .[0] | @base64d | fromjson')
     kid=$(echo "$jwtdec" | jq -r '.kid')
     url=$(printf "$cog_url" "$region" "$userpool")
