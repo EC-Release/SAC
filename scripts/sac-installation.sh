99cb988edc53ba0a93f229bd95462c853cdec9f3
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-03-25 00:23:16 -0700

    Update sac-installation.sh

--- scripts/sac-installation.sh
+++ scripts/sac-installation.sh
@@ -17,67 +17,47 @@ kubectl config view && kubectl get pods && {
     #read csc"?EC_CSC: "
     #read nsc"?Namespace: "
     
-    read -p "EC_CID: " cid
-    read -p "EC_CSC: " csc
-    read -p "Namespace: " nsc
+    #read -p "EC_CID: " cid
+    #read -p "EC_CSC: " csc
+    read -p "Namespace: " SAC_NS
     read -p "ClaimName: " ClaimName
-    read -p "EC_SVC_ID: " EC_SVC_ID
-    read -p "EC_SETTING: " EC_SETTING
-    read -p "EC_ADM_TKN: " EC_ADM_TKN
-    
+    read -p "K8_SECRT_NAME: " K8_SECRT_NAME
     
     #read -p "EC_CID: " cid
     #read -p "EC_CSC: " csc
-    EC_CID=$(printf '%s' "$cid" | base64 | tr '\n' ' ')
-    EC_CSC=$(printf '%s' "$csc" | base64 | tr '\n' ' ')
+    #EC_CID=$(printf '%s' "$cid" | base64 | tr '\n' ' ')
+    #EC_CSC=$(printf '%s' "$csc" | base64 | tr '\n' ' ')
     
-    K8_SECRT_NAME="ec-secret"
+    #K8_SECRT_NAME="ec-secret"
     SAC_MSTR_NAME="sac-mstr"
     SAC_SLAV_NAME="sac-slav"
-    SVC_APP_NAME="svc"
-    SAC_NS="$nsc"
+    #SVC_APP_NAME="svc"
+    #SAC_NS="$nsc"
     
     #EC_ADM_TKN="my-legacy-admin-token"
     #EC_SETTING=$(printf '{"%s":{"ids":["my-aid-1","my-aid-2"],"trustedIssuerIds":["legacy-cf-uaa-url"]}}' "$EC_SVC_ID" | base64 | tr '\n' ' ') 
     #EC_SVC_ID="EC_SVC_ID"
 
-    kubectl delete ingress "${SVC_APP_NAME}"-igs
-    kubectl delete deployments "$SVC_APP_NAME"
-    kubectl delete svc "$SVC_APP_NAME"
     kubectl delete deployments "$SAC_MSTR_NAME"
     kubectl delete svc "$SAC_MSTR_NAME"
     kubectl delete deployments "$SAC_SLAV_NAME"
     kubectl delete svc "$SAC_SLAV_NAME"
-    kubectl delete secrets "$K8_SECRT_NAME"
+    #kubectl delete secrets "$K8_SECRT_NAME"
     
     curl -Ss -o sac.yaml https://raw.githubusercontent.com/ayasuda-ge/sac/main/k8s/sac.yaml
-    sed -i "" "s|{EC_CID}|$EC_CID|g" sac.yaml
-    sed -i "" "s|{EC_CSC}|$EC_CSC|g" sac.yaml
+    #sed -i "" "s|{EC_CID}|$EC_CID|g" sac.yaml
+    #sed -i "" "s|{EC_CSC}|$EC_CSC|g" sac.yaml
     sed -i "" "s|{K8_SECRT_NAME}|$K8_SECRT_NAME|g" sac.yaml
     sed -i "" "s|{SAC_MSTR_NAME}|$SAC_MSTR_NAME|g" sac.yaml
     sed -i "" "s|{SAC_SLAV_NAME}|$SAC_SLAV_NAME|g" sac.yaml
     sed -i "" "s|{SAC_NS}|$SAC_NS|g" sac.yaml
-    sed -i "" "s|{ClaimName}|$ClaimName|g" sac.yaml    
-    
-    curl -Ss -o svc1.1.yml https://raw.githubusercontent.com/ayasuda-ge/service1.x/1.1/k8s/svc1.1.yml
-    sed -i "" "s|{EC_CID}|$EC_CID|g" svc1.1.yml
-    sed -i "" "s|{EC_CSC}|$EC_CSC|g" svc1.1.yml
-    sed -i "" "s|{K8_SECRT_NAME}|$K8_SECRT_NAME|g" svc1.1.yml
-    sed -i "" "s|{SAC_MSTR_NAME}|$SAC_MSTR_NAME|g" svc1.1.yml
-    sed -i "" "s|{SAC_SLAV_NAME}|$SAC_SLAV_NAME|g" svc1.1.yml
-    sed -i "" "s|{SVC_APP_NAME}|$SVC_APP_NAME|g" svc1.1.yml
-    sed -i "" "s|{SAC_NS}|$SAC_NS|g" svc1.1.yml
-    sed -i "" "s|{ClaimName}|$ClaimName|g" svc1.1.yml
-    
-    sed -i "" "s|{EC_ADM_TKN}|$EC_ADM_TKN|g" svc1.1.yml
-    sed -i "" "s|{EC_SETTING}|$EC_SETTING|g" svc1.1.yml
-    sed -i "" "s|{EC_SVC_ID}|$EC_SVC_ID|g" svc1.1.yml
+    sed -i "" "s|{ClaimName}|$ClaimName|g" sac.yaml
     
     kubectl apply -f sac.yaml
     
-    echo completing svc1.1 bootstrap ..
-    sleep 15
-    kubectl apply -f svc1.1.yml
+    #echo completing svc1.1 bootstrap ..
+    #sleep 15
+    #kubectl apply -f svc1.1.yml
     
     exit 0
 }
